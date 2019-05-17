cb07e9e9ddd771c50ddeedcbc46144782cad3343
Kerwin L <48508357+KerwinL@users.noreply.github.com>
2019-05-17 22:49:26 +0800

    增加项目基础文件
    增加项目基础文件

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+
+export default {
+  name: 'App',
+  components:{
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
